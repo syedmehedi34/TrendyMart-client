c0b5c97d369d326489d2e9388da4367ec89f47d5
syedmehedi34 <syedmeehdi34@gmail.com>
2025-07-09 01:38:55 +0600

    footer complete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div className="">
+          <img src={assets?.logo} className="mb-5 w-32" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+880 17317-71000</li>
+            <li>trendymart@gmail.com</li>
+            {/* <li></li> */}
+          </ul>
+        </div>
+      </div>
+
+      {/* copyright section */}
+      <div>
+        <hr className="text-[#E5E7EB]" />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ greatstack.dev - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
