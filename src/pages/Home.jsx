7ebf294176114ab4d0cbeecfd2320eb682f1aaa9
syedmehedi34 <syedmehedi34@gmail.com>
2025-07-08 17:07:58 +0600

    navbar working fine

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold text-center">Welcome to Home Page</h1>
-      <p className="text-lg mt-4">This is the home page of your application.</p>
+    <div className="">
+      <h1>home</h1>
     </div>
   );
 };
