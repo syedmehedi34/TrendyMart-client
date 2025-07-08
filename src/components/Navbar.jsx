aa84a4f4e47300f9c06c22c76238af479832bc57
syedmehedi34 <syedmeehdi34@gmail.com>
2025-07-08 22:56:43 +0600

    latest collection completed and navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} alt="" className="w-36 h-[41px]" />
+      <Link to="/">
+        <img src={assets.logo} alt="" className="w-36 h-[41px]" />
+      </Link>
 
       {/* navlinks  */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
