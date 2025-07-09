1821e8d9b6765aaea17156947a910ff3a9d2a52c
syedmehedi34 <syedmehedi34@gmail.com>
2025-07-09 15:44:29 +0600

    searchbar design completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,12 @@ import { Link, NavLink } from "react-router-dom";
 import { assets } from "../assets/assets.js";
 import { ChevronRight, Menu, Search, ShoppingCart, User } from "lucide-react";
 import { useState } from "react";
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const { setShowSearch } = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -35,7 +39,10 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <Search className="w-5 cursor-pointer" />
+        <Search
+          onClick={() => setShowSearch(true)}
+          className="w-5 cursor-pointer"
+        />
 
         <div className="group relative">
           <User className="w-5 cursor-pointer" />
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -7,8 +7,18 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
 
-  const value = { products, currency, delivery_fee };
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
 
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
