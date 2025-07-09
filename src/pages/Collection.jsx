aa030f364fa6ea5a8af1b8e9714b5ab57a485333
syedmehedi34 <syedmehedi34@gmail.com>
2025-07-09 13:44:08 +0600

    filter implemented in the collection page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -32,9 +32,31 @@ const Collection = () => {
     }
   };
 
+  const applyFilter = () => {
+    let productsCopy = products.slice();
+
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        category.includes(item.category)
+      );
+    }
+
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        subCategory.includes(item.subCategory)
+      );
+    }
+
+    setFilterProducts(productsCopy);
+  };
+
   useEffect(() => {
     setFilterProducts(products);
   }, [products]);
+
+  useEffect(() => {
+    applyFilter();
+  }, [category, subCategory]);
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t border-[#E5E7EB]">
