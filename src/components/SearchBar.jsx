694ed240ab137874c05feeba1c5081b76af25daf
syedmehedi34 <syedmehedi34@gmail.com>
2025-07-09 15:53:20 +0600

    searchbar show and hidden function changed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,25 @@
-/* eslint-disable no-unused-vars */
-import { useContext } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { Search, X } from "lucide-react";
+import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
   const { search, setSearch, showSearch, setShowSearch } =
     useContext(ShopContext);
+  const [visible, setVisible] = useState(false);
 
-  return showSearch ? (
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname.includes("collection")) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, [location]);
+
+  return showSearch && visible ? (
     <div className="border-t border-b border-[#E5E7EB] bg-gray-50 text-center">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
