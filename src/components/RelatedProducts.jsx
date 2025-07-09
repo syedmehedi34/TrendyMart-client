e00e29de3eaf31cd0408caaa45671da274403cf1
syedmehedi34 <syedmehedi34@gmail.com>
2025-07-10 01:00:56 +0600

    add to cart button functionalities updated

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -25,7 +25,7 @@ const RelatedProducts = ({ category, subCategory }) => {
         <Title text1="RELATED" text2="PRODUCTS" />
       </div>
 
-      <div className="border grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
         {related.map((item, index) => (
           <ProductItem
             key={index}
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
@@ -84,7 +84,10 @@ const Product = () => {
             </div>
           </div>
 
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 font-medium hover:bg-gray-800 transition-colors duration-300 rounded cursor-pointer">
+          <button
+            onClick={() => addToCart(productData._id, size)}
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 font-medium hover:bg-gray-800 transition-colors duration-300 rounded cursor-pointer"
+          >
             ADD TO CART
           </button>
           <hr className="mt-8 sm:w-4/5 text-[#E5E7EB]" />
